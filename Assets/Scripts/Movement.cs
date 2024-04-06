c1fd5ed388ac5598fcf96da7284ab1430bc310f8
ALudwig23 <2308313@student.toa.edu.my>
2024-04-06 23:43:25 +0800

    Add project files.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody2D;
+    protected 
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
